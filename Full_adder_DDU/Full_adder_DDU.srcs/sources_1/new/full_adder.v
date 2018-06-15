ee21e83dbf0658166eccac69174a0007c792e44c
Ashutosh Mishra <iashutosh9119@gmail.com>
2018-06-15 09:27:35 +0900

    Add Full_adder_DDU project

---
+++ Full_adder_DDU/Full_adder_DDU.srcs/sources_1/new/full_adder.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.10.2019 13:22:53
+// Design Name: 
+// Module Name: full_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module full_adder(
+    input a,b,cin,
+    output sum,carry
+    );
+    assign sum = a^b^cin;
+    assign carry=(a&b)|(b&cin)|(cin&a);
+endmodule
